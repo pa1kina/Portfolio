67a0ec5dca9bf9dcb37f5357233c5b415778cf8d
pa1kina <p.volskaya@gmail.com>
2025-08-02 00:21:57 -0700

    made rough draft of project cards

--- src/Components/Projects/ProjectCard.tsx
+++ src/Components/Projects/ProjectCard.tsx
@@ -1,11 +1,35 @@
-import "./ProjectsCard.scss"
+import "./ProjectsCard.scss";
 
-const ProjectCard = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+interface ProjectCardProps {
+  title: string;
+  description: string;
+  image: string;
+  imageAlt?: string;
 }
 
-export default ProjectCard
+const ProjectCard = ({
+  title,
+  description,
+  image,
+  imageAlt
+}: ProjectCardProps) => {
+  return (
+    <div className="project-card">
+      <div className="punchholes">
+        <div className="punchhole"></div>
+        <div className="punchhole"></div>
+      </div>
+      <div className="project-card-content">
+        <div className="text">
+          <h3>{title}</h3>
+          <p>{description}</p>
+        </div>
+        <div className="project-image">
+          <img src={image} alt={imageAlt}/>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectCard;
--- src/Components/Projects/Projects.tsx
+++ src/Components/Projects/Projects.tsx
@@ -1,14 +1,25 @@
 import "./Projects.scss";
 import ProjectCard from "./ProjectCard";
+import projectData from "./projects-info.json";
 
 const Projects = () => {
   return (
     <div className="projects" id="projects">
       <div className="projects-title">
         <p>check out my</p>
-        <h1>Projects</h1>
+        <h1>PROJECTS</h1>
       </div>
-      <ProjectCard />
+      <div className="projects-grid">
+        {projectData.map((project) => (
+          <ProjectCard
+            key={project.id}
+            title={project.title}
+            description={project.description}
+            image={project.image}
+            imageAlt={project.imageAlt}
+          />
+        ))}
+      </div>
     </div>
   );
 };
