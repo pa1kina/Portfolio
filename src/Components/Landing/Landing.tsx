a622ee5f1f3a548489e4fd61f9ce3f2cbf4d5ed5
pa1kina <p.volskaya@gmail.com>
2026-02-10 23:51:36 -0800

    added small animation to landing paper

--- src/Components/Landing/Landing.tsx
+++ src/Components/Landing/Landing.tsx
@@ -3,8 +3,8 @@ import "./Landing.scss"
 const Landing = () => {
   return (
     <div className='landing' id='home'>
-      <img className='landing-img' src="/desktop portfolio.png"></img>
-      <img className='landing-img-mobile' src="/portfolio visuals MOBILE.png"></img>
+      <img className='landing-img fade-in-element' src="/desktop portfolio.png"></img>
+      <img className='landing-img-mobile fade-in-element' src="/portfolio visuals MOBILE.png"></img>
       <div className="background-letters">
         <p className="background-letters p">P</p>
         <p className="background-letters v">V</p>
